ad3e2bc3d71bb75adbe9fa03172c4e04580c735c
Ma Liulong <marooncn@163.com>
2018-05-04 22:11:28 +0800

    Update trick.cpp

--- Cplusplus/trick.cpp
+++ Cplusplus/trick.cpp
@@ -4,6 +4,6 @@ bool isEqual = if(fabs(a-b) < tolerance) ? true : false;
 
 // 判断整数是否为奇数，不要用 x%2 != 0，因为x可能为负数
 bool isOdd = if(x%2 == 1) ? true : false;
-// 或者可以通过位运算进行判别
+// 或者可以通过位运算进行判别，不能用x&1==0判别整数是否为偶数，可以用!(x&1)
 if(x&1) ? printf("odd"); : printf("even");
 
