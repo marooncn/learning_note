d1c5971d5d7443be9c53f58db356f36c2346d3a0
Ma Liulong <marooncn@163.com>
2018-05-11 20:05:10 +0800

    Update leetcode.cpp

--- Cplusplus/leetcode.cpp
+++ Cplusplus/leetcode.cpp
@@ -453,3 +453,38 @@ public:
         return prefix;
     }
 };
+
+3Sum
+#include <vector>
+#include <algorithm>
+using std::vector;
+using std::sort;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        if(nums.size() < 3)
+            return result;
+        sort(nums.begin(), nums.end());
+        for(auto i=nums.begin(),last=nums.end(); i<last-2; i++) {
+            auto j = i+1;
+            if(i>nums.begin() && *i==*(i-1)) continue;
+            auto k=last-1;
+            while(j<k) {
+                if(*i+*j+*k < 0) {
+                    ++j;
+                    while(*j==*(j-1) && j<k) j++;
+                } else if(*i+*j+*k > 0) {
+                    k--;
+                    while(*k==*(k+1) && j<k) k--;
+                } else {
+                    result.push_back({*i, *j, *k});
+                    j++; k--;
+                    while(*j==*(j-1) && *k==*(k+1) && j<k) ++j;
+                }
+                }
+            }
+        return result;
+    }
+};  
