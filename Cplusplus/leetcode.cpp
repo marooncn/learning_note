8d73866ae234b0db56da9ca0a9939a01193f4350
Ma Liulong <marooncn@163.com>
2018-06-07 16:54:08 +0800

    Update leetcode.cpp

--- Cplusplus/leetcode.cpp
+++ Cplusplus/leetcode.cpp
@@ -1,3 +1,38 @@
+21. Merge Two Sorted Lists
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        ListNode dummy(0), *tail = &dummy;  //这种表示在链表问题中很常用，见第2题
+        for(; l1!=NULL && l2!=NULL; tail=tail->next) {
+            if(l1->val <= l2->val) {tail->next = l1; l1 = l1->next;}
+            else {tail->next = l2; l2 = l2->next;}
+        }
+        tail->next = (l1==NULL ? l2 : l1);
+        return dummy.next;
+    }
+};
+
+递归解法：
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1->val <= l2->val) {l1->next = mergeTwoLists(l1->next, l2); return l1;}
+        else {l2->next = mergeTwoLists(l1, l2->next); return l2;}
+    }
+};
+
 20. Valid Parentheses
 #include <string>
 #include <stack>
